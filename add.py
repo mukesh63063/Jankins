71eb69471a5b8a25eca2b0baeee6e195e3781bab
mukesh63063 <mukeshprajapatisob@gmail.com>
2025-10-08 14:13:06 +0530

    Update add.py

--- add.py
+++ add.py
@@ -1,12 +1,12 @@
 class Calculator:
-  def __init__(self, a, b):
-    self.a = a
-    self.b = b
-def add(self):
-  return self.a + self.b
-def sub(self):
-  return self.a - self.b
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+    def add(self):
+        return self.a + self.b
+    def sub(self):
+        return self.a - self.b
 
-c = Calculator(20, 10)
+c = Calculator(10,5)
 print(c.add())
 print(c.sub())
